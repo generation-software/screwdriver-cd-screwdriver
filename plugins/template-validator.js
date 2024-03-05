243afc3ac7da741370111be4ff7d89edfc12044c
Jiaqi Liu <jack20220723@gmail.com>
2024-03-05 15:33:25 +0800

    Use forked template validator (#10)

--- plugins/template-validator.js
+++ plugins/template-validator.js
@@ -3,7 +3,7 @@
 const boom = require('@hapi/boom');
 const schema = require('screwdriver-data-schema');
 const templateSchema = schema.api.templateValidator;
-const validator = require('screwdriver-template-validator').parseJobTemplate;
+const validator = require('screwdriver-cd-template-validator').parseJobTemplate;
 
 /**
  * Hapi Template Validator Plugin
